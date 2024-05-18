ac0fadb9bf85dfcdcbedf7387d3e1134e2261e6b
harijoshi <hariojoshi@gmail.com>
2024-05-18 12:00:58 +0545

    add: text styles

--- app/src/main/java/com/example/intomyday/presentation/TextStyles.kt
+++ app/src/main/java/com/example/intomyday/presentation/TextStyles.kt
@@ -1,7 +1,48 @@
 package com.example.intomyday.presentation
 
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.example.intomyday.R
 
 val fontRoboto = FontFamily(Font(R.font.roboto))
+val fontMontserrat = FontFamily(Font(R.font.montserrat))
+
+var h1TextStyle = TextStyle(
+    fontSize = 26.sp,
+    fontFamily = fontMontserrat,
+    fontWeight = FontWeight.Bold
+)
+
+var h2TextStyle = TextStyle(
+    fontSize = 22.sp,
+    fontFamily = fontMontserrat,
+    fontWeight = FontWeight.Bold
+)
+
+var infoTextStyle = TextStyle(
+    fontSize = 20.sp,
+    fontFamily = fontMontserrat,
+    fontWeight = FontWeight.SemiBold
+)
+
+var infoDescTextStyle = TextStyle(
+    fontSize = 14.sp,
+    fontFamily = fontRoboto,
+    fontWeight = FontWeight.Normal
+)
+
+var taskTextStyle = TextStyle(
+    fontSize = 12.sp,
+    fontFamily = fontRoboto,
+    fontWeight = FontWeight.Normal
+)
+
+
+var taskDescTextStyle = TextStyle(
+    fontSize = 18.sp,
+    fontFamily = fontRoboto,
+    fontWeight = FontWeight.Normal
+)
