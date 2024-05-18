50bc8d056d2db4a2b91fe4219f2bba1b646412bf
harijoshi <hariojoshi@gmail.com>
2024-05-18 11:39:30 +0545

    chore: modify theme colors

--- app/src/main/java/com/example/intomyday/ui/theme/Theme.kt
+++ app/src/main/java/com/example/intomyday/ui/theme/Theme.kt
@@ -12,9 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Blue500,
+    secondary = Blue200,
+    background = Blue500,
 )
 
 private val LightColorScheme = lightColorScheme(
