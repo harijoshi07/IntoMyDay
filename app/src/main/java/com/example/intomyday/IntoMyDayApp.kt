cb2366a6ba2bf736dcab0ca4cd5d047c8becf047
harijoshi <hariojoshi@gmail.com>
2024-05-18 10:30:48 +0545

    Initial commit

---
+++ app/src/main/java/com/example/intomyday/IntoMyDayApp.kt
@@ -0,0 +1,9 @@
+package com.example.intomyday
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun IntoMyDayApp(modifier: Modifier = Modifier) {
+
+}
