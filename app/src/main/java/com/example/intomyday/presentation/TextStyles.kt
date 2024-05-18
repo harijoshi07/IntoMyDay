189be4412a4b95e9d12d7d2b9fbcf9b2d9548527
harijoshi <hariojoshi@gmail.com>
2024-05-18 11:55:44 +0545

    modify: fonts added and deleted

---
+++ app/src/main/java/com/example/intomyday/presentation/TextStyles.kt
@@ -0,0 +1,7 @@
+package com.example.intomyday.presentation
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.example.intomyday.R
+
+val fontRoboto = FontFamily(Font(R.font.roboto))
