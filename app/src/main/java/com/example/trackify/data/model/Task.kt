59dd9575ab0a630ae71096007806e1d224f42b6f
harijoshi <hariojoshi@gmail.com>
2024-05-19 18:38:42 +0545

    feat: add time formatting function

--- app/src/main/java/com/example/trackify/data/model/Task.kt
+++ app/src/main/java/com/example/trackify/data/model/Task.kt
@@ -2,6 +2,9 @@ package com.example.trackify.data.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 @Entity(tableName = "tasks_tbl")
 data class Task(
@@ -10,4 +13,18 @@ data class Task(
     val isCompleted: Boolean,
     val startTime: Long,
     val endTime: Long
-)
+) {
+    fun getFormattedTime(): String {
+        val startTimeFormat = formatTime(startTime)
+        val endTimeFormat = formatTime(endTime)
+        return "$startTimeFormat - $endTimeFormat"
+    }
+
+
+    private fun formatTime(time: Long): String {
+        val timeFormatter = SimpleDateFormat("hh:mm a", Locale.getDefault())
+        val date = Date(time)   //converts time in long into date object
+        return timeFormatter.format(date)
+
+    }
+}
