9b0f3f5b8b839081482afeb1b15547cb5904ca99
harijoshi <hariojoshi@gmail.com>
2024-05-19 18:03:09 +0545

    modify: TrackifyApp and HomeScreen

--- app/src/main/java/com/example/trackify/TrackifyApp.kt
+++ app/src/main/java/com/example/trackify/TrackifyApp.kt
@@ -1,24 +1,68 @@
 package com.example.trackify
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.trackify.presentation.fontRoboto
+import com.example.trackify.presentation.h1TextStyle
 import com.example.trackify.ui.screen.HomeScreen
+import com.example.trackify.ui.theme.TrackifyTheme
+import com.example.trackify.ui.theme.Yellow
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TrackifyApp(modifier: Modifier = Modifier) {
 
     Scaffold(topBar = {
-        TopAppBar(title = { Text(text = stringResource(id = R.string.app_name)) })
+        TopAppBar(
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = MaterialTheme.colorScheme.background
+            ),
+            modifier = Modifier.padding(end = 8.dp),
+            title = { Text(text = stringResource(id = R.string.app_name), style = h1TextStyle) },
+            actions = {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_fire),
+                    contentDescription = null,
+                    tint = Yellow,
+                    modifier = Modifier.size(32.dp)
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(
+                    text = "12", style = TextStyle(
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        fontStyle = FontStyle.Normal,
+                        fontFamily = fontRoboto
+                    )
+                )
+            }
+        )
     }) {
-        HomeScreen(modifier = Modifier.padding(it))
+        Column(modifier = Modifier.padding(it)) {
+            HomeScreen()
+        }
 
     }
 
@@ -27,6 +71,9 @@ fun TrackifyApp(modifier: Modifier = Modifier) {
 @Preview
 @Composable
 private fun PrevTrackifyApp() {
-    TrackifyApp()
+    TrackifyTheme(darkTheme = true, dynamicColor = false) {
+        TrackifyApp()
+    }
+
 
 }
--- app/src/main/java/com/example/trackify/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/example/trackify/ui/screen/HomeScreen.kt
@@ -1,16 +1,23 @@
 package com.example.trackify.ui.screen
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.trackify.R
+import com.example.trackify.presentation.h2TextStyle
 import com.example.trackify.ui.components.InfoComponent
 import com.example.trackify.ui.theme.Blue
 import com.example.trackify.ui.theme.Green
@@ -18,28 +25,47 @@ import com.example.trackify.ui.theme.Green
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
 
-    Row(
-        horizontalArrangement = Arrangement.spacedBy(16.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier.fillMaxWidth()
+    Column(
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(12.dp)
     ) {
-        InfoComponent(
-            title = "Completed",
-            desc = "3/6",
-            icon = R.drawable.ic_task_list,
-            backgroundColor = Green,
-            modifier = Modifier.weight(1f)
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+            //.padding(12.dp)
+        ) {
+            InfoComponent(
+                title = "Completed",
+                desc = "3/6",
+                icon = R.drawable.ic_task_list,
+                backgroundColor = Green,
+                modifier = Modifier.weight(1f)
 
+            )
+
+            InfoComponent(
+                title = "Free Time",
+                desc = "3 hours",
+                icon = R.drawable.ic_clock,
+                backgroundColor = Blue,
+                modifier = Modifier.weight(1f)
+
+            )
+
+        }
+
+        Text(
+            text = stringResource(R.string.today_s_task),
+            style = h2TextStyle,
+            color = Color.White,
+            modifier = Modifier.padding(16.dp)
         )
 
-        InfoComponent(
-            title = "Free Time",
-            desc = "3 hours",
-            icon = R.drawable.ic_clock,
-            backgroundColor = Blue,
-            modifier = Modifier.weight(1f)
-
-        )
     }
 
 }
