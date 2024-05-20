3b5941181c079dc31cb5417b40e57cc686bab695
harijoshi <hariojoshi@gmail.com>
2024-05-20 11:39:48 +0545

    feat: create a task repository

---
+++ app/src/main/java/com/example/trackify/repositories/TaskRepository.kt
@@ -0,0 +1,27 @@
+package com.example.trackify.repositories
+
+import com.example.trackify.data.local.TaskDao
+import com.example.trackify.domain.model.Task
+import kotlinx.coroutines.flow.Flow
+
+class TaskRepository(private val dao: TaskDao) {
+    suspend fun insert(task: Task) {
+        dao.insertTask(task)
+    }
+
+    suspend fun update(task: Task) {
+        dao.updateTask(task)
+    }
+
+    suspend fun delete(task: Task) {
+        dao.deleteTask(task)
+    }
+
+    fun getTaskById(id: Int): Flow<Task> {
+        return dao.getTaskById(id)
+    }
+
+    fun getAllTasks(): Flow<List<Task>> {
+        return dao.getAllTasks()
+    }
+}
