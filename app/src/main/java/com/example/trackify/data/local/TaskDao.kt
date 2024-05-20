6d7f1b90fe90e0f67bfda94857e453181824a703
harijoshi <hariojoshi@gmail.com>
2024-05-20 09:26:35 +0545

    feat: implement task dao interface

---
+++ app/src/main/java/com/example/trackify/data/local/TaskDao.kt
@@ -0,0 +1,37 @@
+package com.example.trackify.data.local
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.trackify.domain.model.Task
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TaskDao {
+    @Insert
+    suspend fun insertTask(task: Task) {
+
+    }
+
+    @Update
+    suspend fun updateTask(task: Task) {
+
+    }
+
+    @Delete
+    suspend fun deleteTask(task: Task) {
+
+    }
+
+    @Query("SELECT * FROM tasks_tbl WHERE id=:id")
+    //With Flow as the return type, you receive notification whenever the data in the database changes.
+    // The Room keeps this Flow updated for you, which means you only need to explicitly get the data once
+    fun getTaskById(id: Int): Flow<Task>
+
+    @Query("SELECT * FROM tasks_tbl")
+    fun getAllTasks(): Flow<List<Task>>
+
+
+}
