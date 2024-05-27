79a5837d31ff772ae8b0a3493ff16c5ca5ea5b81
harijoshi <hariojoshi@gmail.com>
2024-05-27 09:01:17 +0545

    feat: implement delete dialog ui

--- app/src/main/java/com/example/trackify/ui/add_edit_screen/components/ConfirmDeleteDialog.kt
+++ app/src/main/java/com/example/trackify/ui/add_edit_screen/components/ConfirmDeleteDialog.kt
@@ -1,12 +1,111 @@
 package com.example.trackify.ui.add_edit_screen.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.trackify.R
+import com.example.trackify.presentation.h2TextStyle
+import com.example.trackify.presentation.taskTextStyle
+import com.example.trackify.ui.theme.Blue200
+import com.example.trackify.ui.theme.Blue500
+import com.example.trackify.ui.theme.Red
 
 @Composable
 fun ConfirmDeleteDialog(modifier: Modifier = Modifier) {
 
+    ElevatedCard(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(40.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.secondary
+        ),
+
+        ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth(1f)
+                .padding(8.dp)
+        ) {
+            Text(
+                text = "Delete Task?", style = TextStyle(
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    fontFamily = FontFamily.Monospace,
+                    color = Color.White
+                )
+            )
+            Image(
+                painter = painterResource(id = R.drawable.delete),
+                contentDescription = null,
+                modifier = Modifier.size(128.dp)
+            )
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 12.dp)
+            ) {
+                Button(
+                    onClick = { /*TODO*/ },
+                    shape = RoundedCornerShape(8.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Blue200.copy(alpha = 0.5f)
+                    )
+                ) {
+                    Text(
+                        text = "Cancel", style = TextStyle(
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.SemiBold,
+                            fontFamily = FontFamily.Monospace,
+                            color = Color.White
+                        )
+                    )
+                }
+                Button(
+                    onClick = { /*TODO*/ },
+                    shape = RoundedCornerShape(8.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Red
+                    )
+                ) {
+                    Text(
+                        text = "Delete", style = TextStyle(
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.SemiBold,
+                            fontFamily = FontFamily.Monospace,
+                            color = Color.Black
+                        )
+                    )
+                }
+            }
+        }
+    }
 }
 
 @Preview
