0995b1c005bb0aed74ce3101542ac06b7a08e7e7
harijoshi <hariojoshi@gmail.com>
2024-05-30 21:25:17 +0545

    feat: complete delete task func

--- app/src/main/java/com/example/trackify/ui/add_edit_screen/components/ConfirmDeleteDialog.kt
+++ app/src/main/java/com/example/trackify/ui/add_edit_screen/components/ConfirmDeleteDialog.kt
@@ -43,7 +43,7 @@ fun ConfirmDeleteDialog(
     modifier: Modifier = Modifier
 ) {
 
-    Dialog(onDismissRequest = { }) {
+    Dialog(onDismissRequest = {onClose() }) {
         ElevatedCard(
             modifier = Modifier
                 .fillMaxWidth(),
