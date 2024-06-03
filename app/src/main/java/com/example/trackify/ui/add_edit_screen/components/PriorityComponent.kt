f87c59b497413792961bcf72b8619abd297579c4
harijoshi <hariojoshi@gmail.com>
2024-06-03 19:36:55 +0545

    feat: task priority UI

---
+++ app/src/main/java/com/example/trackify/ui/add_edit_screen/components/PriorityComponent.kt
@@ -0,0 +1,44 @@
+package com.example.trackify.ui.add_edit_screen.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.trackify.presentation.h2TextStyle
+import com.example.trackify.ui.theme.Red
+
+@Composable
+fun PriorityComponent(title: String, backgroundColor: Color, modifier: Modifier = Modifier) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(8.dp),
+        colors = CardDefaults.cardColors(containerColor = backgroundColor)
+    ) {
+        Text(
+            text = title,
+            style = h2TextStyle,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentSize(Alignment.Center)
+                .padding(16.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewPriorityComponent() {
+    PriorityComponent(title = "", backgroundColor = Red)
+
+}
